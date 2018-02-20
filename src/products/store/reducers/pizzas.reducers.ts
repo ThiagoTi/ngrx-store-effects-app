21e1c312e73faad4b9610edd469ee517d5514640
Thiago Trevisan <thiago@digitaldoc.com.br>
2018-02-20 16:48:33 -0300

    Our first @Effect

--- src/products/store/reducers/pizzas.reducers.ts
+++ src/products/store/reducers/pizzas.reducers.ts
@@ -8,58 +8,12 @@ export interface PizzaState {
 }
 
 export const initialState: PizzaState = {
-  data: [
-    {
-      name: "Seaside Surfin'",
-      toppings: [
-        {
-          id: 6,
-          name: 'mushroom'
-        },
-        {
-          id: 7,
-          name: 'olive'
-        },
-        {
-          id: 2,
-          name: 'bacon'
-        },
-        {
-          id: 3,
-          name: 'basil'
-        },
-        {
-          id: 1,
-          name: 'anchovy'
-        },
-        {
-          id: 8,
-          name: 'onion'
-        },
-        {
-          id: 11,
-          name: 'sweetcorn'
-        },
-        {
-          id: 9,
-          name: 'pepper'
-        },
-        {
-          id: 5,
-          name: 'mozzarella'
-        }
-      ],
-      id: 2
-    }
-  ],
+  data: [],
   loaded: false,
   loading: false
 };
 
-export function reducer(
-  state = initialState,
-  action: fromPizzas.PizzasAction
-): PizzaState {
+export function reducer(state = initialState, action: fromPizzas.PizzasAction): PizzaState {
   switch (action.type) {
     case fromPizzas.LOAD_PIZZAS: {
       return {
@@ -68,10 +22,13 @@ export function reducer(
       };
     }
     case fromPizzas.LOAD_PIZZAS_SUCCESS: {
+      const data = action.payload;
+
       return {
         ...state,
         loading: false,
-        loaded: true
+        loaded: true,
+        data
       };
     }
     case fromPizzas.LOAD_PIZZAS_FAIL: {
@@ -84,3 +41,7 @@ export function reducer(
   }
   return state;
 }
+
+export const getPizzasLoading = (state: PizzaState) => state.loading;
+export const getPizzasLoaded = (state: PizzaState) => state.loaded;
+export const getPizzas = (state: PizzaState) => state.data;
