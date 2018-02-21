f6fe7e043b9a1c6b2b526ec60ecbd1b7fa1fad3f
Thiago Trevisan <thiago@digitaldoc.com.br>
2018-02-21 08:19:26 -0300

    Optimizing Data Structures with Entities

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -11,14 +11,15 @@ export const reducers: ActionReducerMap<ProductsState> = {
 
 export const getProductsState = createFeatureSelector<ProductsState>('products');
 
-
-
 // pizzas state
 export const getPizzaState = createSelector(
   getProductsState,
   (state: ProductsState) => state.pizzas
 );
 
-export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
-export const getAllPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
-export const getAllPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
+export const getPizzasEntities = createSelector(getPizzaState, fromPizzas.getPizzasEntities);
+export const getAllPizzas = createSelector(getPizzasEntities, entities => {
+  return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
+});
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
